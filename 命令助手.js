5709e4766bd761be0e97f600b73097facaa6a205
ProjectXero <projectxero@163.com>
2018-10-02 15:18:21 +0800

    fix(每日问题): 隐藏的关于部分挡住文本框的bug

--- 命令助手.js
+++ 命令助手.js
@@ -15589,6 +15589,47 @@ MapScript.loadModule("AndroidBridge", {
 	}]
 });
 
+MapScript.loadModule("DexPlugin", {
+	load : function(packageName, mainClass) {
+		var r, pi, dir, cx, cl;
+		try {
+			pi = ctx.getPackageManager().getPackageInfo(packageName, 0);
+			dir = pi.applicationInfo.publicSourceDir;
+			cx = org.mozilla.javascript.Context.getCurrentContext();
+			cl = Packages.dalvik.system.DexClassLoader(
+				dir,
+				ctx.getDir("dex", 0).getAbsolutePath(),
+				null,
+				cx.getApplicationClassLoader()
+			);
+		} catch(e) {Log.e(e)}
+		if (!cl) return null;
+		r = Object.create(this);
+		r.classes = {};
+		r.packageInfo = pi;
+		r.classLoader = cl;
+		if (mainClass) r.mainClass = r.get(mainClass);
+		return r;
+	},
+	get : function(className) {
+		var cx, cls;
+		if (className) {
+			if (className in this.classes) {
+				cls = this.classes[className];
+			} else {
+				try {
+					cx = org.mozilla.javascript.Context.getCurrentContext();
+					cls = cx.getWrapFactory().wrapJavaClass(cx, MapScript.global, this.classLoader.loadClass(className));
+				} catch(e) {Log.e(e)}
+				this.classes[className] = cls;
+			}
+		} else {
+			cls = this.mainClass;
+		}
+		return cls;
+	}
+});
+
 MapScript.loadModule("NeteaseAdapter", {
 	onCreate : function() {
 		MapScript.loadModule("getMinecraftVersion", this.getMinecraftVersion);
@@ -16167,7 +16208,7 @@ MapScript.loadModule("WSServer", {
 						self.print(_e + "\n" + _e.stack, new G.ForegroundColorSpan(Common.theme.criticalcolor));
 					}
 				}
-				self.ready(s);
+				self.ready(_s);
 			}
 			self.onClose = function() {
 				G.ui(function() {try {
