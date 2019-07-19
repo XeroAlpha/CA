93246d62e7bbd70057b7e213ba93a0a39dfd78b5
ProjectXero <projectxero@163.com>
2019-07-19 14:54:04 +0800

    chore: 更改主文件名为main.js

--- tools/tasks/getSourceCode.js
+++ tools/tasks/getSourceCode.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const loader = require("../loader");
 module.exports = function(context, args) {
-	var result = loader.load(context.cwd + "/命令助手.js", null, { buildConfig : context.buildConfig });
+	var result = loader.load(context.cwd + "/main.js", null, { buildConfig : context.buildConfig });
 	fs.writeFileSync(context.cwd + "/build/export.js", result);
 	return result;
 }
