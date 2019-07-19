870808d51f231ffb9811b7d7559f201c6d8e32ef
ProjectXero <projectxero@163.com>
2019-07-19 20:57:51 +0800

    chore: gradle的输出会立即输出而不是运行结束后

--- tools/tasks/initShellConfig.js
+++ tools/tasks/initShellConfig.js
@@ -1,6 +1,6 @@
+const fs = require("fs");
+const readConfig = require("../readconfig");
+
 module.exports = function(context, args) {
-	context.shellConfig = {
-		jarsignerPath : "C:\\Program Files\\Java\\jdk1.8.0_171\\bin\\jarsigner.exe",
-		zipalignPath : "C:\\Users\\Administrator\\AppData\\Local\\Android\\Sdk\\build-tools\\28.0.0\\zipalign.exe"
-	};
+	context.shellConfig = readConfig(fs.readFileSync("./config/shell.txt", "utf-8"));
 }
--- tools/tasks/shellGradle.js
+++ tools/tasks/shellGradle.js
@@ -3,16 +3,17 @@ const child_process = require("child_process");
 module.exports = function(context, args) {
 	return new Promise(function(resolve, reject) {
 		var cmd = "gradlew --console plain " + args;
-		console.log("Running " + cmd);
+		console.log("Running " + args);
 		var child = child_process.exec(cmd, {
 			cwd : context.shellcwd
 		}, (error, stdout, stderr) => {
 			if (error) {
 				reject(error);
 			} else {
-				console.log(stdout);
 				resolve();
 			}
 		});
+		child.stdout.pipe(process.stdout);
+		child.stderr.pipe(process.stderr);
 	});
 }
