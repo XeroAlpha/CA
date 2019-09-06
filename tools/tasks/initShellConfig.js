9764f7dcb9bf7a543cf64e5e92ddba8d16116506
ProjectXero <projectxero@163.com>
2019-09-07 00:51:55 +0800

    fix: Android构建工具dex目录变更

--- tools/tasks/initShellConfig.js
+++ tools/tasks/initShellConfig.js
@@ -3,5 +3,5 @@ const readConfig = require("../readconfig");
 
 module.exports = function(context, args) {
 	context.shellConfig = readConfig(fs.readFileSync("./config/shell.txt", "utf-8"));
-	context.shellConfig.dexPath = "/app/build/intermediates/transforms/dexMerger/release/0/classes.dex";
+	context.shellConfig.dexPath = "/app/build/intermediates/dex/release/mergeDexRelease/out/classes.dex";
 }
