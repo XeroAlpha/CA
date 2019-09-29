6486c5388971f9d4e00d5ee601c2737f0d878457
ProjectXero <projectxero@163.com>
2019-09-30 07:47:30 +0800

    feat: 支持NetworkUtils.requestApi函数

--- modules/network/NetworkUtils.js
+++ modules/network/NetworkUtils.js
@@ -139,5 +139,72 @@ MapScript.loadModule("NetworkUtils", {
 			return r;
 		}
 		return o;
-	})()
+	})(),
+	requestApi : function(method, url) {
+		var regexp = /:(\w+)/g, argCount = arguments.length, argIndex = 2;
+		var params, query, content, result;
+		if (regexp.test(url) && argIndex < argCount) {
+			params = arguments[argIndex];
+			url = url.replace(regexp, function(match, key) {
+				if (key in params) {
+					return encodeURIComponent(params[key]);
+				} else {
+					return "";
+				}
+			});
+			argIndex++;
+		}
+		if (method == "GET" || method == "HEAD" || method == "DELETE") {
+			query = arguments[argIndex];
+		} else {
+			if (argCount - argIndex > 1) {
+				query = arguments[argIndex];
+				argIndex++;
+			}
+			content = arguments[argIndex];
+		}
+		if (query) {
+			url += "?" + this.toQueryString(query);
+		}
+		try {
+			result = JSON.parse(NetworkUtils.request(
+				url,
+				method,
+				content ? JSON.stringify(content) : null,
+				content ? "application/json" : null
+			));
+		} catch(e) {
+			throw NetworkUtils.parseError(e);
+		}
+		return result.result;
+	},
+	errorMessages : Object.create(null), 
+	parseError : function(e) {
+		var json, message;
+		if (!e.errorMessage) return e;
+		if (e.responseCode == 500) {
+			return "内部错误";
+		} else if (e.responseCode == 503) {
+			return "服务不可用";
+		}
+		try {
+			json = JSON.parse(e.errorMessage);
+		} catch(err) {/* Not a json */}
+		if (!json) return e;
+		message = this.errorMessages[json.error];
+		if (!message) {
+			message = "未知错误(" + json.error + ")\n" + json;
+		}
+		return message;
+	},
+
+	addErrorMessages : function(messages) {
+		var i;
+		for (i in messages) {
+			if (i in this.errorMessages) {
+				Log.throwError(new Error("Error message " + i + " already exists."));
+			}
+			this.errorMessages[i] = messages[i];
+		}
+	}
 });
